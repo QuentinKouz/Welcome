0dd674d1d545e19ccdbf3aa009dd71155ad95fa2
Quentin Kouzmitch <quentinvegeto@gmail.com>
2023-04-21 15:02:50 +0200

    welcome(bob)

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,5 +2,9 @@ package ilu2;
 
 public class Welcome {
 	
-	public static String welcome(String input)
+	public static String welcome(String input) {
+		input = input.substring(0,1).toUpperCase() 
+				+ input.substring(1);
+		return "Hello, " + input;
+	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -2,13 +2,22 @@ package ilu2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
 
+	Welcome wel;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		wel = new Welcome();
+	}
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_welcome1() {
+		assertEquals("Hello, Bob",wel.welcome("bob"));
 	}
 
 }
